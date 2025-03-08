ab1d7aeac720438b9d403be20f0c34429a08ab6a
oldpondplop <76494676+oldpondplop@users.noreply.github.com>
2025-03-08 17:03:47 +0200

    add expo-auth-session, expo-crypto, expo-secure-store plugins

--- frontend-rn/app.config.ts
+++ frontend-rn/app.config.ts
@@ -2,8 +2,8 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  slug: 'my-app',
-  name: 'My App',
+  slug: 'frontend-rn',
+  name: 'frontend-rn',
   extra: {
     API_URL: process.env.API_URL || 'http://localhost:8000',
   },
