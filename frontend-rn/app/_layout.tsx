0d1c8f2c4deeb3a1654d6e4051a4b21ae76eb078
oldpondplop <76494676+oldpondplop@users.noreply.github.com>
2025-03-12 00:41:09 +0200

    new autogen client and working form

--- frontend-rn/app/_layout.tsx
+++ frontend-rn/app/_layout.tsx
@@ -6,14 +6,31 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 import Constants from 'expo-constants';
-import { OpenAPI } from '@/src/client';
+import { client } from '@/src/client/client.gen';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useReactQueryDevTools } from '@dev-plugins/react-query';
+import { AuthProvider, getTokenFromStorage } from '@/context/auth';
 
+client.setConfig({
+  baseUrl: Constants.expoConfig?.extra?.API_URL || 'http://localhost:8000',
+  auth: async () => {
+    const token = await getTokenFromStorage();
+    return token;
+  },
+});
 
-OpenAPI.BASE = Constants.expoConfig?.extra?.API_URL || 'http://localhost:8000';
+client.interceptors.request.use(async (request) => {
+  console.log(`Request: ${request}`);
+  return request;
+});
+
+client.interceptors.response.use((response) => {
+  console.log(`Response: ${response}`);
+  return response;
+});
+
 const queryClient = new QueryClient();
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -40,10 +57,15 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
+        <AuthProvider>
+          <Stack>
+            {/* Public routes */}
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            {/* Protected routes */}
+            <Stack.Screen name="(app)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </AuthProvider>
         <StatusBar style="auto" />
       </ThemeProvider>
     </QueryClientProvider>
